d29d4145da16dd884cc7e50e38ba83f5355dd1c7
JoshuaHenriques <joshuahenriques95@gmail.com>
2023-11-27 18:42:20 -0500

    update

--- src/structs.rs
+++ src/structs.rs
@@ -1,10 +1,13 @@
+// use actix_web::{
+//     body::BoxBody, http::header::ContentType, HttpRequest, HttpResponse, Responder,
+// };
 use std::{
     cell::Cell,
     sync::atomic::AtomicUsize,
     sync::Arc,
     sync::Mutex,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 pub struct AppState {
     pub app_name: String,
     pub mut_name: String,
@@ -12,6 +15,7 @@ pub struct AppState {
 
 pub struct AppStateWithCounterMutex {
     // <- Mutex is necessary to mutate safely across threads
+    // Mutex blocks the current the current thread    
     pub mut_counter: Mutex<i32>, 
 }
 
@@ -33,3 +37,22 @@ pub struct Info {
 pub struct FormData {
     pub username: String,
 }
+
+#[derive(Serialize)]
+pub struct MyObj {
+    name: &'static str,
+}
+
+// Responder
+// impl Responder for MyObj {
+//     type Body = BoxBody;
+
+//     fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+//         let body = serde_json::to_string(&self).unwrap();
+
+//         // create response and set content type
+//         HttpResponse::Ok()
+//             .content_type(ContentType::json())
+//             .body(body)
+//     }
+// }
